8f3d78a20af840f63d669dc22bbc579a09ace477
ngai to <ngai_to_lo@hotmail.com>
2017-08-25 12:40:52 -0700

    changed the fots fixed spellign mistake

--- Modules/conf.py
+++ Modules/conf.py
@@ -97,7 +97,8 @@ html_theme = 'alabaster'
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
-# html_theme_options = {}
+html_theme_options = {"font_family" : 'arial',
+                      "page_width"  : '940 px'}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
